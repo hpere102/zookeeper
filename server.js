c07ca431f99553f832c033a5db84f0af96a3a70f
danny p <danny@dannys-MacBook-Pro.local>
2021-11-25 14:17:07 -0500

    404 added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const { animals } = require('./data/animals');
 
 
-
+// trait filter function
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -46,12 +46,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
   });
 
 
